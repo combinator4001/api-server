be41a6a00b65aef9bcf3acb463d8dc82cde62fe5
Hamed Feizabadi <hamedfz19@gmail.com>
2021-11-05 18:58:15 +0330

    company crud boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { RegisterModule } from './register/register.module';
 import { PersonModule } from 'src/person/person.module';
+import { CompanyModule } from './company/company.module';
 @Module({
-    imports: [RegisterModule, PersonModule]
+    imports: [RegisterModule, PersonModule, CompanyModule]
 })
 export class AppModule {}
