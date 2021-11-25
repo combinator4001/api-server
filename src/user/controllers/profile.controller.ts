72b6052d3b8f7dd551a46a3cc00c8a5f5016969d
Hamed Feizabadi <hamedfz19@gmail.com>
2021-11-25 19:41:56 +0330

    upload file added

--- src/user/controllers/profile.controller.ts
+++ src/user/controllers/profile.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Controller, Get, HttpCode, HttpException, HttpStatus, Put, Request, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
+import { ApiBadRequestResponse, ApiBody, ApiConsumes, ApiCreatedResponse, ApiHeader, ApiOperation, ApiPayloadTooLargeResponse, ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "src/auth/general/jwt-auth.guard";
+import { ProfileService } from "../services/profile.service";
+import { FileInterceptor } from '@nestjs/platform-express';
+import { isFileExtensionSafe, removeFile, saveImageToStorage } from './../helpers/profile-image.storage';
+import { join } from 'path';
+import { ImageUploadDto } from './../dtos/image-upload.dto';
 
-@ApiTags('User')
+@ApiTags('User / Profile')
 @Controller()
 export class ProfileController{
+    constructor(private profileService : ProfileService){}
+
     @Get('/:username/profile')
     getProfile(){
 
@@ -11,6 +19,67 @@ export class ProfileController{
 
     @Get('/me/profile')
     getMyProfile(){
+
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Put('/image')
+    @HttpCode(200)
+    @UseInterceptors(
+      FileInterceptor('image', saveImageToStorage)
+    )
+    @ApiOperation({description : 'Updates profile image.'})
+    @ApiHeader({name : 'Authorization'})
+    @ApiConsumes('multipart/form-data')
+    @ApiBody({
+      description: 'Attach the image file',
+      type: ImageUploadDto
+    })
+    @ApiCreatedResponse({description : 'Profile image updated!'})
+    @ApiBadRequestResponse({description : 'File must be a png, jpg/jpeg. |  Invalid file.'})
+    @ApiPayloadTooLargeResponse({description : 'File too large. File should be less than 300KiB.'})
+    async updateImage(@UploadedFile() file: Express.Multer.File, @Request() req){
+      const fileName = file?.filename;
+      
+      if(!fileName){
+        //400
+        //wrong file extension
+        throw new HttpException({
+          statusCode : HttpStatus.BAD_REQUEST,
+          message : 'File must be an iamge in png, jpg/jpeg format!'
+        }, HttpStatus.BAD_REQUEST);
+      }
+      else{
+        //file extension name is correct 
+        //but needs to be checked if it is safe or not
+  
+        const imagesFolderPath = join(process.cwd(), 'temp/profile-images');
+        const fullImagePath = join(imagesFolderPath + '/' + file.filename);
+  
+        if(await isFileExtensionSafe(fullImagePath)){
+  
+          //200
+          await this.profileService.updateImage(fullImagePath);
+          this.profileService.updateUserImagePathById(req.user.id, fileName);  
+          removeFile(fullImagePath);
+
+          return {
+            statusCode : HttpStatus.OK,
+            message : 'Profile image updated!',
+            imageLink : "arvan.storage"
+          };
+        }
+        else{
+  
+          //400
+          //extension is correct but file is not valid | unsafe extension
+          removeFile(fullImagePath);
+          throw new HttpException({
+            statusCode : HttpStatus.BAD_REQUEST,
+            message : 'Invalid file!'
+          }, HttpStatus.BAD_REQUEST);
+        }
+      }
 
     }
 }
--- src/user/services/profile.service.ts
+++ src/user/services/profile.service.ts
@@ -1,6 +1,71 @@
 import { Injectable } from "@nestjs/common";
+import { basename, join } from "path";
+import { PrismaService } from "src/app/prisma.service";
+const { S3Client, PutObjectCommand, CreateBucketCommand } = require('@aws-sdk/client-s3');
+const fs = require('fs');
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Injectable()
 export class ProfileService{
-    
+    constructor(private prisma : PrismaService){}
+
+    /**
+     * Uploads the image to the storage.
+     * @param fullImagePath 
+     */
+    async updateImage(fullImagePath : string){
+        // Create an S3 client service object
+        const s3 = new S3Client({
+            region: 'default',
+            endpoint: process.env.endpoint as string,
+            credentials: {
+                accessKeyId: process.env.accessKey as string,
+                secretAccessKey: process.env.secretKey as string,
+            }
+        });
+
+        const uploadParams = {
+            Bucket: 'combinator-profile-images', // bucket name
+            Key: 'object-name', // the name of the selected file
+            ACL: 'public-read', // 'private' | 'public-read'
+            Body: 'BODY',
+        };
+
+        // BODY (the contents of the uploaded file - leave blank/remove to retain contents of original file.)
+        //FILE_NAME (the name of the file to upload (if you don't specify KEY))
+        const fullFilePath = fullImagePath;
+
+        // call S3 to retrieve upload file to specified bucket
+        const run = async () => {
+            // Configure the file stream and obtain the upload parameters
+            const fileStream = fs.createReadStream(fullFilePath);
+            fileStream.on('error', function (err) {
+                console.log('File Error', err);
+            });
+            uploadParams.Key = basename(fullFilePath);
+            // call S3 to upload file to specified bucket
+            uploadParams.Body = fileStream;
+
+            try {
+                const data = await s3.send(new PutObjectCommand(uploadParams));
+                console.log('Success', data);
+            } catch (err) {
+                console.log('Error', err);
+            }
+        };
+
+        await run();
+    }
+
+    async updateUserImagePathById(userId, imageName){
+        await this.prisma.user.update({
+          where : {
+            id : userId
+          },
+          data : {
+            imageName : imageName
+          }
+        })
+      }
 }
